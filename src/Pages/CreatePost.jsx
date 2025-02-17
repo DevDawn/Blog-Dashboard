f4bf702dbe4288efa22e12064f30f545c9316f2a
DevDawn <cobhamdawn00@gmail.com>
2025-02-17 23:33:30 +0100

    Bloggg

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const CreatePost = () => {
-  return (
-    <div>
-      <h1 className='text-center'>Hello I am Create Post</h1>
-    </div>
-  )
-}
 
-export default CreatePost
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newErrors = {};
+  
+    if (title === "") {
+      newErrors.title = "Title is required";
+    } else if (title.length < 5) {
+      newErrors.title = "Title must be at least 5 characters";
+    } else if (title.length > 100) {
+      newErrors.title = "Title must be at most 100 characters";
+    }
+  
+    if (content === "") {
+      newErrors.content = "Content is required";
+    } else if (content.length < 5) {
+      newErrors.content = "Content must be at least 5 characters";
+    }
+  
+    if (image === "") {
+      newErrors.image = "Image is required";
+    }
+  
+    if (date === "") {
+      newErrors.date = "Date is required";
+    }
+  
+    if (category === "") {
+      newErrors.category = "Category is required";
+    }
+  
+    if(Object.keys(newErrors).length > 0){
+      setErrors(newErrors);
+    } else {
+      alert("Blog Sent");
+    }
+  };
+
+  return (
+    <div className='bg-blue-300 px-10 py-10 rounded-xl mx-auto my-20' style={{ maxWidth: '600px' }}
+    >
+      <form onSubmit={handleSubmit}>
+        <h1 className='text-3xl text-center font-bold text-blue-900'>Create Blog</h1>
+        <div className='flex flex-col mt-5'>
+          <label className='font-bold'>Title:</label>
+          <input type="text" placeholder='Enter Post Title' className="mt-1 p-2 border-3 border-blue-400 outline-blue-600 rounded" value={title} onChange={(event)=>setTitle(event.target.value)}/>
+          {errors.title && <p className='text-red-500'>{errors.title}</p>}
+        </div>
+        <div className='flex flex-col mt-5'>
+          <label className='font-bold'>Content:</label>
+          <textarea placeholder='Enter Blog Content' className="mt-1 p-2 border-3 border-blue-400 outline-blue-600 rounded" value={content} onChange={(event)=>setContent(event.target.value)}/>
+          {errors.content && <p className='text-red-500'>{errors.content}</p>}
+        </div>
+        <div className='flex flex-col mt-5'>
+          <label className='font-bold'>Enter Image URL:</label>
+          <input type="url" placeholder='Image URL' className="mt-1 p-2 border-3 border-blue-400 outline-blue-600 rounded" value={image} onChange={(event)=>setImage(event.target.value)}/>
+          {errors.image && <p className='text-red-500'>{errors.image}</p>}
+        </div>
+        <div className='flex flex-col mt-5'>
+          <label className='font-bold'>Published Date:</label>
+          <input type="date" className="mt-1 p-2 border-3 border-blue-400 outline-blue-600 rounded" value={date} onChange={(event)=>setDate(event.target.value)}/>
+          {errors.date && <p className='text-red-500'>{errors.date}</p>}
+        </div>
+        <div className='mt-5'>
+          <label className='font-bold'>Category:</label>
+          <div className="flex justify-between">
+            <label>
+              <input type="radio" value="Entrepreneurship" checked={category === "Entrepreneurship"} onChange={() => setCategory("Entrepreneurship")} className='cursor-pointer'/> Entrepreneurship
+            </label>
+            <label>
+              <input type="radio" value="Technology" checked={category === "Technology"} onChange={() => setCategory("Technology")} className='cursor-pointer' /> Technology
+            </label>
+            <label>
+              <input type="radio" value="Innovation" checked={category === "Innovation"} onChange={() => setCategory("Innovation")} className='cursor-pointer'/> Innovation
+            </label>
+          </div>
+          {errors.category && <p className='text-red-500'>{errors.category}</p>}
+        </div>
+        <button type="submit" className="mt-8 w-full py-3 bg-blue-900 text-white rounded cursor-pointer">Submit Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from "react";
+
+const blogData = [
+  {
+    id: 3,
+    title:
+      "Making wearable medical devices more patient-friendly with Professor Esther Rodriguez-Villegas from Acurable",
+    image:
+      "https://techcrunch.com/wp-content/uploads/2022/05/found-2022-featured.jpg?w=430&h=230&crop=1",
+    author: "Darrell Etherington",
+    published_date: "2023-10-01",
+    content:
+      "Welcome back to Found, where we get the stories behind the startups. This week, our old friend Darrell Etherington joins Becca Szkutak to talk with Professor Esther Rodriguez-Villegas from Acurable...",
+    category: "Tech",
+  },
+  {
+    id: 4,
+    title:
+      "Rainforest raises $8.5M to help software companies embed financial services, payments",
+    image:"https://techcrunch.com/wp-content/uploads/2015/02/shutterstock_128451140.jpg?w=430&h=230&crop=1",
+    author: "Mary Ann Azevedo",
+    published_date: "2023-10-01",
+    content:
+      "In November 2019, Andreessen Horowitz General Partner Angela Strange famously declared that, “Every company will be a fintech company.” Specifically, Strange projected that — in the not-too-d...",
+    category: "Innovation",
+  },
+  {
+    id: 5,
+    title:
+      "Pow.bio says biomanufacturing is broken and its continuous fermentation tech will fix it",
+    image:
+      "https://techcrunch.com/wp-content/uploads/2023/10/Pow-Lab2.jpg?w=430&h=230&crop=1",
+    author: "Christine Hall",
+    published_date: "2023-10-01",
+    content:
+      "Pow.bio intends to bring down the costs associated with biomanufacturing by reimagining of fermentation facility operations.",
+    category: "Technology",
+  },
+  {
+    id: 6,
+    title:
+      "Recapitalization, $60M Series D support growth of e-commerce financier Clearco",
+    image:
+      "https://techcrunch.com/wp-content/uploads/2022/07/GettyImages-1314165902.jpg?w=430&h=230&crop=1",
+    author: "Christine Hall",
+    published_date: "2023-10-01",
+    content:
+      "Today is news marks a turnaround for a company that is had its share of ups and downs over the past year.",
+    category: "Entrepreneurship",
+  },
+];
 
 const Homepage = () => {
   return (
-    <div>
-      <h1 className='text-center mt-5 cursor-pointer hover:underline' >Hello from HomePage</h1>
-    </div>
-  )
-}
+    <div className="py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mb-10">
+      {blogData.length != 0 ?
+        blogData.map((blogPost, index) => (
+          <div key={index} className="shadow hover:shadow-lg rounded-xl m-5 relative">
+           <div className="w-full h-40 object-cover">
+           <img src={blogPost.image} alt={blogPost.title} className="w-full h-40"/>
+           </div>
+           <div className="px-4 py-3">
+            <h1 className="font-bold text-md mb-2">{blogPost.title}</h1>
+            <p className="text-sm text-ellipsis text-gray-600">{blogPost.content.slice(0,100)}...</p>
 
-export default Homepage
+              <p className="py-4 text-xs text-gray-800">{blogPost.published_date}</p>
+
+            <div className="absolute top-3 p-2 px-4 bg-blue-900 rounded-full w-max">
+              <p className="text-sm text-white">{blogPost.category}</p>
+            </div>
+
+           </div>
+          </div>
+        ))
+      
+      :
+      <div className="flex h-screen w-full justify-center items-center">No blog post...</div>
+      }
+    </div>
+  );
+};
+
+export default Homepage;
